22e0e972794dad71b22c530e3f4123fea6a59e13
suchitrasahu996-creator <suchitrasahu996@gmail.com>
2026-01-18 20:01:55 +0530

    added the answer

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app =express();
 const PORT = 3000;
+app.use(express.json());
+app.use("orders",require("./routes/oders.routes"))
 
 app.listen(PORT,()=>{
     console.log (`Server is running on port ${PORT}`);
